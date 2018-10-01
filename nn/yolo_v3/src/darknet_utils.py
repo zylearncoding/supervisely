b95a58825655f0d9b0b9af5bd44566bcf1b275c2
Alf162 <nikolay.kurochka@gmail.com>
2018-10-01 20:43:21 +0300

    add comments

--- nn/yolo_v3/src/darknet_utils.py
+++ nn/yolo_v3/src/darknet_utils.py
@@ -50,7 +50,7 @@ class METADATA(Structure):
     
 
 #lib = CDLL("/home/pjreddie/documents/darknet/libdarknet.so", RTLD_GLOBAL)
-lib = CDLL("/workdir/src/darknet/libdarknet.so", RTLD_GLOBAL)
+lib = CDLL("/workdir/src/libdarknet.so", RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
---
+++ tutorials/10_yolo_inference/docker/run.sh
@@ -0,0 +1,8 @@
+docker build -t 10_yolo_inference:latest . && \
+docker run --rm -it --init \
+	--runtime=nvidia \
+	-v `pwd`/../../../supervisely_lib:/workdir/src/supervisely_lib \
+	-v `pwd`/../../../nn/yolo_v3/src:/workdir/src/yolo_v3 \
+	-v `pwd`/../src:/workdir/src \
+	-v `pwd`/../data:/sly_task_data \
+	-p 8888:8888 10_yolo_inference:latest bash
